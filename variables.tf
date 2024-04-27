204a532de34096ee68ee54f9ef26941a2600674d
ervinita98@gmail.com <ervinita98@gmail.com>
2024-04-27 14:34:58 +0200

    giving proper name to some variables

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  default       = "ca-vpnpeering-rg"
-  description   = "Resource group name."
-}
-
-variable "resource_group_location" {
-  default       = "West US"
-  description   = "Location of the resource group."
-}
